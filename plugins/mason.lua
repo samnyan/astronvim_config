972f6db9e99ae0b013f2c5afa33c49b6dba062d8
samnyan <4137880+samnyan@users.noreply.github.com>
2023-05-03 09:15:14 +0800

    Sync

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -8,7 +8,7 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       if not opts.ensure_installed then opts.ensure_installed = {} end
       require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
+        "lua_ls",
       })
     end,
   },
@@ -21,7 +21,7 @@ return {
       if not opts.ensure_installed then opts.ensure_installed = {} end
       require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         -- "prettier",
-        -- "stylua",
+        "stylua",
       })
     end,
   },
@@ -32,7 +32,7 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       if not opts.ensure_installed then opts.ensure_installed = {} end
       require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "python",
+        "python",
       })
     end,
   },
